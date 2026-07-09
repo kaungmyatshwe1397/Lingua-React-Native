7b8b63e46acf09d3cbfd8c1629f11b0861f2f3c0
Kaung Myat Shwe <kaungmyatshwe1@example.com>
2026-07-10 04:00:22 +0700

    feat: update Index component text and modify H1 color for improved branding

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue">
-      <Text className="text-h1">Hello Native Wind</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-h1">Lingua</Text>
     </View>
   );
 }
